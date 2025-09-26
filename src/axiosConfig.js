77035b65cc00448bc84f3aa3d6c31692b13b9302
Nafiwiz98 <nafiyadassefa@gmail.com>
2025-09-26 11:17:45 +0300

    updated axiosConfig

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosBase = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: " https://forum-backend-7cdw.onrender.com/api",
 });
 
 export default axiosBase
