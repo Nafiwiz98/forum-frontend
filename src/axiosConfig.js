78dfb45de2308a2a4321eb078792d47ad7b40078
Nafiwiz98 <nafiyadassefa@gmail.com>
2025-09-29 18:39:03 +0300

    backend registration

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosBase = axios.create({
-  baseURL: " https://forum-backend-7cdw.onrender.com/api",
+  baseURL: "https://forum-backend-7cdw.onrender.com/api",
 });
 
 export default axiosBase
